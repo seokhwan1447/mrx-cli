805452cae8ac08a5ff2853b09128c24a41364c01
seokhwankim <seokhwan.kim@makinarocks.ai>
2020-04-24 14:01:33 +0900

    version update

--- bin/mrx.js
+++ bin/mrx.js
@@ -2,7 +2,7 @@
 const program = require('commander');
  
 program
-  .version('1.0.2')
+  .version('1.0.7')
   .command('create <app-name>')
   .action(function (name) {
     require('../lib/create')(name);
